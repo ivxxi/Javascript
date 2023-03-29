0dc6a04599f86a55bb10e93cdf3b335d94e7be76
ivxxi <crystalalice21@gmail.com>
2023-03-29 20:56:13 +0300

    adds calculate bill function

---
+++ tip-calculator/script.js
@@ -0,0 +1,31 @@
+//get global access to all input / div her
+//bill input, tip input, number of people div and per person
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv  = document.getElementById('perPersonTotal')
+
+//get number of people from number of people div
+ let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+
+ // calculate total bill per person
+ const calculateBill = () => {
+    const bill = Number(billInput.value)
+
+    const tipPercentage = Number(tipInput.value)/100
+
+    const tipAmount = bill * tipPercentage
+
+    const total = tipAmount + bill
+
+    const perPersonTotal = total / numberOfPeople
+
+    perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
+ }
+
+ //split bill between more people
+ const increasePeople = () => {}
+
+ //split bill between less people
+ const decreasePeople = () => {} 
+ //guard clause: can't go below 1
