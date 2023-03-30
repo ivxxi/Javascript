03d63f2b7dfd06a4b0fecc2ded9d4886b738e040
ivxxi <crystalalice21@gmail.com>
2023-03-30 23:32:27 +0300

    adds decimal function

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -16,6 +16,6 @@ const calculateBmi = () => {
         result.innerText = "Please provide a valid weight";
     } else {
     const bmi = weightKg / (heightCm * heightCm * 0.0001)
-    result.innerText = `${bmi}`
+    result.innerText = `${bmi.toFixed(2)}`
     }
 }
