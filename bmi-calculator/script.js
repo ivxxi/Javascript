6358084da640f37a34f14efeda4946dcd60425e3
ivxxi <crystalalice21@gmail.com>
2023-03-30 23:23:06 +0300

    adds condition in js file

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -8,6 +8,14 @@ const calculateBmi = () => {
     const heightCm = Number(height.value)
     const weightKg = Number(weight.value)
 
+    if((heightCm === '') || (heightCm < 0) || (isNaN(heightCm))){
+        //NaN !== NaN
+        result.innerText = "Please provide a valid height";
+        
+    } else if (weightKg === '' || weightKg < 0 || isNaN(weightKg)){
+        result.innerText = "Please provide a valid weight";
+    } else {
     const bmi = weightKg / (heightCm * heightCm * 0.0001)
     result.innerText = `${bmi}`
+    }
 }
